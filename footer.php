04d380d4b81106fc05bfcb333720394ccbcc981a
trygvevalbo <trygvevalbo@gmail.com>
2019-03-22 23:38:59 +0100

    footer implementering
    footer implimentering

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <?php wp_footer();?>
 <footer>
+	<div class='sosialemedierlink'>
+		<a href='#'>Instagram</a>
+		<a href='#'>Facebook</a>
+		<a href='#'>Youtube</a>
+	</div>
+	
+	<div>
+	<h2>Copyright © 2019 design</h2>
+	</div>
 </footer>
 </div>
 </body>

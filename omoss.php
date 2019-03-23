710ad761a14024a81b21684fcb8c29da62280a7d
trygvevalbo <trygvevalbo@gmail.com>
2019-03-23 18:19:44 +0100

    om oss

---
+++ omoss.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+<div class='presentasjon'>
+
+</div>
+<?php get_footer();?>
--- page.php
+++ page.php
@@ -1,10 +1,10 @@
  <?php get_header();?>
-<div class="container pt-5 pb-5">
+ <link rel="stylesheet" type="text/css" href="omoss.css">.
+<div class='presentasjon'>
 	<h1><?php the_title();?></h1>
+ <?php echo do_shortcode('[tmm name="maritime"]')?>
 	
-	<?php if(have_posts()) : while(have_posts()): the_post();?>
-		<?php the_content();?>
-	<?php endwhile; endif;?>
-	</div>
 
+
+</div>
 <?php get_footer();?>

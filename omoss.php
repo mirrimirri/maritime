db3c3d96bca66a5c972dfb8cf9f02acfd7d0f6c6
trygvevalbo <trygvevalbo@gmail.com>
2019-03-26 20:19:22 +0100

    slit med å laste in n css til hver enkel side

--- omoss.php
+++ omoss.php
@@ -1,5 +1,15 @@
 <?php /*Template Name: omoss*/?>
 <?php get_header();?>
+<div class='ommaritime'>
+    <h1>Maritime</h1>
+	<p>Lorem ipsum dolor sit amet, no duo consulatu forensibus. Te vel illum sonet, vix in tamquam euripidis. Qui natum quodsi alienum ex. Nibh recusabo per ei, nisl stet omnes in per, an solet pertinacia pri.
+
+Ius sint vitae ne, salutatus gloriatur an pro. Ex vel putant fabulas argumentum, vidisse consulatu no pri. Est prima dolorum ne, usu id case vocent reformidans, no mei audire probatus. At semper quaeque probatus ius, esse inciderint cum ne, nam modus exerci putant ei. Eum pertinax explicari sententiae in, vis at zril tempor possim.</p>
+</div>
+
+</div>
+<div>
+
 <div class='presentasjon'>
  <?php echo do_shortcode('[tmm name="maritime"]')?>
 </div>

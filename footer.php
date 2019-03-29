3951c6d1300c829c3a6d9af380f14127ab76404d
carokbrek <225304@usn.no>
2019-03-29 20:26:27 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,54 @@
 <?php wp_footer();?>
 <footer>
-	<div class='sosialemedierlink'>
-		<a href='#'>Instagram</a>
-		<a href='#'>Facebook</a>
-		<a href='#'>Youtube</a>
-	</div>
-	
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.fa {
+  padding: 5px;
+  font-size: 50px;
+  width: 70px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 5px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-snapchat-ghost {
+  background: #fffc00;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+
+</style>
+</head>
+<body>
+
+<h2>Følg oss</h2>
+
+<!-- Add font awesome icons -->
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-snapchat-ghost"></a>
+
+     
 	<div>
-	<h2>Copyright © 2019 design</h2>
+	<h2>Copyright © 2019 Septa Design</h2>
 	</div>
 </footer>
 </div>

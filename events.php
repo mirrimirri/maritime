fa6dfaba5294e7a31483e2bc5398fb0d6a2c0cad
linnkarolina <46923037+linnkarolina@users.noreply.github.com>
2019-04-02 10:49:24 +0200

    Update events.php

--- events.php
+++ events.php
@@ -4,7 +4,7 @@
  <?php echo do_shortcode('[tmm name="maritime"]')?>
 </div>
 
-<div class='sdsfaas'>
-	
+<div class='events'>
+<?php echo do_shortcode('[facebook_events col=”2″ posts_per_page=”12″ category=”cat1,cat2″ past_events=”yes” order=”desc” orderby=”post_title” start_date=”2017-12-25″ end_date=”2018-12-25″ ]')?>	
 </div>
 <?php get_footer();?>

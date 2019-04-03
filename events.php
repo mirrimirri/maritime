3741009e44b083562fad76218e4f694a00e692e0
Cornerbay <46921134+Cornerbay@users.noreply.github.com>
2019-04-03 13:45:52 +0200

    Endret fildelingssystem

--- events.php
+++ events.php
@@ -10,12 +10,31 @@
 
 <?php 
 
-echo do_shortcode('[MMFileList <ul class=”mmm-list”> /../../members/]');
 
+/*
+Må lage følgende mapper for at dette skal fungere:
 
+wp-content\uploads\public
+wp-content\uploads\members
+wp-content\uploads\admin
+
+Kan lages med plugin: Media Library Folders for WordPress 
+*/
+
+//kode for å vise fram fillisten som er tilgjengelig for alle
+echo do_shortcode('[MMFileList folder="../../public/" <ul class=”mmm-list”> /]');
+
+//kode for å vise filliste som er tilgjengelig for registrerte brukere, må endre if-statement
 if($_SESSION['loggetinn']==true)
 {
-	//echo do_shortcode('[MMFileList <ul class=”mmm-list”> /]');
+	echo do_shortcode('[MMFileList folder="../../members/" <ul class=”mmm-list”> /]');
+
+}
+
+//kode for å vise filliste for admin, må endre if-statement
+if($_SESSION['loggetinn']==true)
+{
+	echo do_shortcode('[MMFileList folder="../../admin/" <ul class=”mmm-list”> /]');
 
 }
 

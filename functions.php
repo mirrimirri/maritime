c6fb6f1253096380fb6a3b48cad7cfdcad82cd43
trygvevalbo <trygvevalbo@outlook.com>
2019-04-03 11:11:48 +0200

    stil til skjema

--- functions.php
+++ functions.php
@@ -33,7 +33,11 @@
 	 }
 	 
 	  if($slug=='nettbutikk'){
-	   wp_enqueue_style('style', get_template_directory_uri() . 'style/nettbutikk.css', array(), false, 'all'); //laster css nettbutikk
+	   wp_enqueue_style('style', get_template_directory_uri() . '/style/nettbutikk.css', array(), false, 'all'); //laster css nettbutikk
+	 }
+	  
+	  if($slug=='minside'){
+	   wp_enqueue_style('style', get_template_directory_uri() . '/style/minside.css', array(), false, 'all'); //laster css nettbutikk
 	 }
 	  
 	

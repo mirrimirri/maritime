5d869cc9b9efa675b132fc29afa03f397577d1db
linnkarolina <46923037+linnkarolina@users.noreply.github.com>
2019-04-09 13:31:51 +0200

    event og filopplastning

--- events.php
+++ events.php
@@ -1,11 +1,11 @@
 <?php /*Template Name: events*/?>
 <?php get_header();?>
-<div class='presentasjon'>
- <?php echo do_shortcode('[tmm name="maritime"]')?>
-</div>
 
-<div class='sdsfaas'>
-	
+<div class='mellom'>
+
+<div class='events'>
+	<h1> Kommende events</h1>
+<?php echo do_shortcode(''); ?>
 </div>
 
 <?php 
@@ -20,9 +20,11 @@ wp-content\uploads\admin
 
 Kan lages med plugin: Media Library Folders for WordPress 
 */
-
+echo" <div class='filopplasting'>";
+echo"<h2> fil nedlastninger </h2>";
 //kode for å vise fram fillisten som er tilgjengelig for alle
 echo do_shortcode('[MMFileList folder="../../public/" <ul class=”mmm-list”> /]');
+
 
 //kode for å vise filliste som er tilgjengelig for registrerte brukere, må endre if-statement
 if($_SESSION['loggetinn']==true)
@@ -37,7 +39,8 @@ if($_SESSION['loggetinn']==true)
 	echo do_shortcode('[MMFileList folder="../../admin/" <ul class=”mmm-list”> /]');
 
 }
-
+echo"</div>";
+echo"</div>";
 ?>
 
 <?php get_footer();?>

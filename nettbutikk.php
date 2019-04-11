7a785bd45f39a170a4b05c5bcaddd8c0edf0939c
linnkarolina <46923037+linnkarolina@users.noreply.github.com>
2019-04-11 14:27:43 +0200

    små endringer i nettbutikken

--- nettbutikk.php
+++ nettbutikk.php
@@ -1,10 +1,14 @@
 <?php /*Template Name: nettbutikk*/
-get_header(); 
+get_header();
+?>
+<div class='produkt'> 
+<?php
 $x=1;
 while ($x<1000){
  echo do_shortcode("[product_page id='$x']"); 
  $x++;	
 }
+echo"</div>";
 get_footer();
 ?>
 

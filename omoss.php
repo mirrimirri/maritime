7e2a53ec9e96082721cc354428be6ed7ed997dff
trygvevalbo <trygvevalbo@outlook.com>
2019-04-21 21:16:22 +0200

    stylet om oss

--- omoss.php
+++ omoss.php
@@ -1,15 +1,16 @@
 <?php /*Template Name: omoss*/?>
 <?php get_header();?>
+<div class='mellom'>
+<div class='hoved'>
 
-
-<div class='ommaritime'>
    <?php if(have_posts()) : while(have_posts()): the_post();?>
 	<div class='post'>
 	<h1><?php the_title();?></h1>
 		<?php the_content();?>
 		</div>
 	<?php endwhile; endif;?>
-	</div>
+	
+</div>
 	
 
 
@@ -56,5 +57,6 @@
 print("<h1> Kontakt oss </h1>");
 echo do_shortcode('[CONTACT_FORM_TO_EMAIL id="1"]')?>
 
+</div>
 </div>
 <?php get_footer();?>

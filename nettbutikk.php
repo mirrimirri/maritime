d076b13483f284f8ea2b045e8871eac06b1642c0
trygvevalbo <trygvevalbo@outlook.com>
2019-04-23 12:49:42 +0200

    pimpa styling på nettbutikk
    nesten helt magisk

--- nettbutikk.php
+++ nettbutikk.php
@@ -1,10 +1,15 @@
 <?php /*Template Name: nettbutikk*/
 get_header(); 
+echo "<div class='mellom'>";
+echo "<h1>Nettbutikk</h1>";
 $x=1;
+echo "<div class='produkt'>";
 while ($x<1000){
- echo do_shortcode("[product_page id='$x']"); 
+ echo do_shortcode("[products ids='$x']"); 
  $x++;	
 }
+echo "</div>";
+echo "</div>";
 get_footer();
 ?>
 

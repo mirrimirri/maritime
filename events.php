1df82089f58d047ce1a72ec9ffddccc4d91f729b
Cornerbay <46921134+Cornerbay@users.noreply.github.com>
2019-05-08 13:28:56 +0200

    Lagt til ikon bak filer

--- events.php
+++ events.php
@@ -23,7 +23,7 @@ Kan lages med plugin: Media Library Folders for WordPress
 echo" <div class='filopplasting'>";
 echo"<h2> Filnedlastninger </h2>";
 //kode for å vise fram fillisten som er tilgjengelig for alle
-echo do_shortcode('[MMFileList folder="../../public/" <ul class=”mmm-list”>/]');
+echo do_shortcode('[MMFileList folder="../../public/" format="li"]<div class="mmm-list"><a href="{url}"">{name} </a></div>[/MMFileList]');
 
 
 //kode for å vise filliste som er tilgjengelig for registrerte brukere, må endre if-statement
@@ -34,8 +34,7 @@ if ( in_array( 'author', (array) $user->roles ) )
 
 {
 	echo "<h2>Medlemsfiler</h2>";
-	echo do_shortcode('[MMFileList folder="../../members/" <ul class=”mmm-list”> /]');
-
+	echo do_shortcode('[MMFileList folder="../../members/" format="li"]<div class="mmm-list"><a href="{url}"">{name} </a></div>[/MMFileList]');
 }
 
 //kode for å vise filliste for admin, må endre if-statement
@@ -44,7 +43,7 @@ $user = wp_get_current_user();
 if ( in_array( 'administrator', (array) $user->roles ) ) 
 {
 	echo "<h2>Administratorfiler</h2>";
-	echo do_shortcode('[MMFileList folder="../../admin/" <ul class=”mmm-list”> /]');
+echo do_shortcode('[MMFileList folder="../../admin/" format="li"]<div class="mmm-list"><a href="{url}"">{name} </a></div>[/MMFileList]');
 
 }
 echo"</div>";

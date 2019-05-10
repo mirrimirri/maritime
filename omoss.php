68d83bd5fd34bc21e14ec0d23cedda991157d3d1
trygvevalbo <trygvevalbo@outlook.com>
2019-05-10 11:22:16 +0200

    style om oss

--- omoss.php
+++ omoss.php
@@ -5,31 +5,35 @@
 <div class='hoved'>
 
    <?php if(have_posts()) : while(have_posts()): the_post();?>
-	<div class='post'>
+	<div class='posts' style="display:block;">
 	<h1><?php the_title();?></h1>
 		<?php the_content();?>
 		</div>
 	<?php endwhile; endif;?>
-	
+	</div>
+
+	</div>
 
 
 
 
 
 <div class='presentasjon'>
-<h1> Om oss </h1>
+<h1> Medlemmer </h1>
 
  <?php 
 
 echo do_shortcode('	[tc-team-members teamid="124"]');	
  ?>
 	
-</div>
+
 </div>
 
 <div class='samarbeidspartnere'>
 <h1> Samarbeidspartnere </h1>
+
 	<?php echo do_shortcode('[sponsors description=yes title=yes]')?>
+
 </div>
 
 <div class='kontaktoss'>
